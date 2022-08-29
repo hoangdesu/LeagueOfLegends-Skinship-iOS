7c509b8d4209f62d1f2eacb994d6993c6d77ea03
Hoang Nguyen <38454021+hoangdesu@users.noreply.github.com>
2022-08-29 11:56:11 +0700

    fix: fix missing intro splash video

--- LeagueOfLegends-Skinship/Views/SplashScreenView.swift
+++ LeagueOfLegends-Skinship/Views/SplashScreenView.swift
@@ -12,38 +12,26 @@ struct SplashScreenView: View {
     
     @Binding var appState: String
     
-    let splashScreenDuration = 1.0 // use 8.0 for production
+    let splashScreenDuration = 8.0 // use 8.0 for production
     
     let video = "intro"
     @State var introPlayer = AVPlayer()
-
+    
     
     var body: some View {
         ZStack {
-            //            Text("Splash Screen")
-            //                .onAppear {
-            //                    withAnimation(.easeIn(duration: 1.5)) {
-            //                        //                self.isActive
-            //                    }
-            //                }
-            
-            //            let player = AVPlayer(url: URL(fileURLWithPath: Bundle.main.path(forResource: "IMG_0226", ofType: "mp4")!))
-            
             Color(.black)
                 .ignoresSafeArea(.all)
             
-            //            VideoPlayer(player: AVPlayer(url:  Bundle.main.url(forResource: "intro", withExtension: "mp4")!))
-            //                .frame(height: 400)
             VideoPlayer(player: introPlayer)
                 .onAppear() {
-                    introPlayer = AVPlayer(url: Bundle.main.url(forResource: "intro", withExtension: "mp4")!)
+                    introPlayer = AVPlayer(url: Bundle.main.url(forResource: "splash-intro", withExtension: "mp4")!)
                     introPlayer.volume = 1.0
-//                    introPlayer.show
                     introPlayer.play()
                 }
             
                 .scaleEffect(1.5)
-                
+            
         }
         .onAppear {
             DispatchQueue.main.asyncAfter(deadline: .now() + self.splashScreenDuration) {
